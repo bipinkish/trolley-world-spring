d9e6b1242f955503c0193027205306c646dbffef
Bipin Kishore <bipinkishore01@gmail.com>
2025-11-10 09:28:20 +0530

    changed security

--- src/main/java/com/bipinkish/store/config/SecurityConfig.java
+++ src/main/java/com/bipinkish/store/config/SecurityConfig.java
@@ -66,7 +66,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));  // or your real frontend URL(s)
+        config.setAllowedOrigins(List.of(
+                "https://trolley-world-api-production.up.railway.app",
+                "https://trolley-world-ui.vercel.app"  // example if you have frontend
+        ));  // or your real frontend URL(s)
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
